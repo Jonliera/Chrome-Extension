b7022a16c70152500b72a1a30c3f3ccc51de83ab
Jonathan Liera <jonliera1@gmail.com>
2023-06-18 20:46:35 -0700

    add template literals exercise

--- test.js
+++ test.js
@@ -5,3 +5,11 @@ container.innerHTML = <button onclick="buy()">Buy </button>;
 function buy() {
   container.innerHTML += "<p>Thank you for buying</p>";
 }
+
+//template literals
+
+const recipient = "James";
+
+const email = `Hey ${recipient} How is it going? Cheers! ${sender}`;
+
+const sender = "Jonathan";
